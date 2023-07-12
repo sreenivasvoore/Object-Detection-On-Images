95eef938e5ee86684c2aba186434b7fe1e2fd91e
sreenivasvoore <vlns88@gmail.com>
2023-07-11 17:43:35 -0700

    Add files via upload

---
+++ sunset.js
@@ -0,0 +1,43 @@
+img = "";
+cocoStatus = "";
+objects = [];
+function preload() {
+    img = loadImage('Sunset.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+    if (cocoStatus != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected" + objects.length;
+            text(objects[i].label + objects[i].confidence, objects[i].x, objects[i].y);
+            fill('#ff0000');
+            stroke("red");
+            noFill();
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function modelLoaded() {
+    console.log("Model Loaded Successfully!");
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
